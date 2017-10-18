93ee66f7e1ea458f7c8c0cced8f10bfbda8417ee
Dev Workstation <EMAIL@gmail.com>
2017-10-18 10:13:33 -0600

    create method to make api call to service

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,11 @@
   class SearchController < ApplicationController
     def index
-      @stations = NRELService.new(zipcode).retrieve_stations
+      @conn = Faraday.new(url: "https://developer.nrel.gov/") do |faraday|
+        faraday.adapter  Faraday.default_adapter
+      end
+
+      response = @conn.get("api/alt-fuel-stations/v1.json?api_key=#{ENV['api_key']}&fuel_type=LPG,ELEC&zip=80203")
+      raw_data = JSON.parse(response.body, symbolize_names: true)
+      binding.pry
     end
   end
