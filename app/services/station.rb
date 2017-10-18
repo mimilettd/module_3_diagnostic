9b913301d54f7232dcaa2c82d8da3e1f9993601f
Dev Workstation <EMAIL@gmail.com>
2017-10-18 11:04:26 -0600

    complete PORO for station

--- app/services/station.rb
+++ app/services/station.rb
@@ -1,6 +1,12 @@
 class Station
   def initialize(attrs = {})
-    binding.pry
-
+    @name = attrs[:station_name]
+    @street = attrs[:street_address]
+    @city = attrs[:city]
+    @state = attrs[:state]
+    @zip = attrs[:zip]
+    @fuel_type = attrs[:fuel_type_code]
+    @distance = attrs[:distance]
+    @access_times = attrs[:access_days_time]
   end
 end
