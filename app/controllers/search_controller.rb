387a86692f5766c872a487ab950b31ce486d9155
Dev Workstation <EMAIL@gmail.com>
2017-10-18 10:44:09 -0600

    refactor api call

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,11 @@
   class SearchController < ApplicationController
     def index
+      zip = params["q"]
       @conn = Faraday.new(url: "https://developer.nrel.gov/") do |faraday|
         faraday.adapter  Faraday.default_adapter
       end
 
-      response = @conn.get("api/alt-fuel-stations/v1.json?api_key=#{ENV['api_key']}&fuel_type=LPG,ELEC&zip=80203")
+      response = @conn.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['api_key']}&fuel_type=LPG,ELEC&location=#{zip}&radius=6")
       raw_data = JSON.parse(response.body, symbolize_names: true)
       binding.pry
     end
